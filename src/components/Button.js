f4f678e9d6b66d8620d4f0bedc037e1ecde59405
Roxolana <roxolanapavlyk@gmail.com>
2019-04-29 12:03:01 +0200

    add href

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function Button({ text }) {
+export default function Button({ text, href, path }) {
   return (
     <div>
-      <button className="mainButton">{text}</button>
+      <button className="mainButton">
+        <a href={href}>{text}</a>
+      </button>
     </div>
   );
 }
