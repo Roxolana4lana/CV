c026d9f41dc6111e5bac0feefe57e3e59199b897
Roxolana <roxolanapavlyk@gmail.com>
2019-04-29 13:17:24 +0200

    Updates

---
+++ precache-manifest.f6f546374ed876b414f11adf756ab1af.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfdfda66f0a234b92c24fdfb65b5688",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "249314429da7bec5d4e8",
+    "url": "/CV/static/css/main.02394d8e.chunk.css"
+  },
+  {
+    "revision": "d6f4e89128decc90e89a",
+    "url": "/CV/static/js/2.d6a05f3b.chunk.js"
+  },
+  {
+    "revision": "e74737181927f029f444",
+    "url": "/CV/static/js/3.c811695c.chunk.js"
+  },
+  {
+    "revision": "249314429da7bec5d4e8",
+    "url": "/CV/static/js/main.aaebd1b5.chunk.js"
+  },
+  {
+    "revision": "e771d0bf3e5578adf13f",
+    "url": "/CV/static/js/runtime~main.d5960ae4.js"
+  },
+  {
+    "revision": "f2d9255add0a6ac915c64d0e291e4bdc",
+    "url": "/CV/static/media/im5.f2d9255a.png"
+  },
+  {
+    "revision": "69c672b26d9116eb0f68cb33cb012e43",
+    "url": "/CV/static/media/image1.69c672b2.jpg"
+  },
+  {
+    "revision": "8ac277292caf704929368d783ecca0f0",
+    "url": "/CV/static/media/image2.8ac27729.jpg"
+  },
+  {
+    "revision": "4e7391148b2d28dff6e7227df0f0323e",
+    "url": "/CV/static/media/image3.4e739114.jpg"
+  },
+  {
+    "revision": "bff31293d79da91d0ff38a3fa1b917cb",
+    "url": "/CV/static/media/image4.bff31293.png"
+  },
+  {
+    "revision": "19845b66db3f1c5940d002e356685b85",
+    "url": "/CV/static/media/image6.19845b66.jpg"
+  },
+  {
+    "revision": "e7171fbe71361335e4ef7d1ef36ba9a5",
+    "url": "/CV/static/media/myImage.e7171fbe.jpg"
+  }
+]);
